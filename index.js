5bf678b283b1252e1cbc397de4bdf7d487035dbe
Erkka Pynnönen <erkka.pynnonen@grano.fi>
2017-06-27 11:34:51 +0300

    server improvements

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(3000, () => {
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+const server = app.listen(app.get('port'), () => {
   console.log(
     'Express server listening on port %d in %s mode',
-    server.address().port,
+    app.get('port'),
     app.settings.env
   );
 });

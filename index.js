423f0e7c81b1b6b09090a0df873ee0d0789a1c30
Erkka Pynnönen <erkka.pynnonen@grano.fi>
2017-06-27 12:08:40 +0300

    Added week number and week number range queries

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const moment = require('moment');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,4 +20,56 @@ const server = app.listen(app.get('port'), () => {
 
 app.get('/', (req, res) => {
   res.send('you have no power here');
+  return;
+});
+
+app.post('/', (req, res) => {
+  let input = req.body.text;
+  let output = {
+    response_type: "ephemeral",
+    title: false,
+    attachments: []
+  };
+
+  let defaultLocale = 'fi';
+  let locale = defaultLocale;
+  let dateFormat = 'l';
+  let rg = {
+    isWeekNumber: new RegExp(/^\d{1,2}(\s\d{1,2})?$/)
+  };
+
+  moment().locale(locale);
+
+  // return current week
+  if (!input) {
+    output.title = "This is week number " + moment().week();
+  }
+  else if (rg.isWeekNumber.test(input)) {
+    let parts = input.split(" ");
+    let date = moment();
+    date.locale(locale);
+
+    date.week(parts[0]);
+    output.title = "Week: " + parts[0];
+    date.startOf("week");
+    output.attachments.push({
+      title: "Starts on",
+      text: date.format(dateFormat),
+      short: true
+    });
+
+    date.week(parts[parts.length-1]);
+    date.endOf("week");
+    output.attachments.push({
+      title: "Ends on",
+      text: date.format(dateFormat),
+      short: true
+    });
+  }
+  else {
+    output.title = "You are not making sense, Betsy";
+  }
+
+  res.json(output);
+  return;
 });
